6a1518f05471f49e05b80fa4b5e60784c9713b79
Fuji-Vanilli <ijufillivan@gmail.com>
2024-07-06 12:37:18 +0000

    security front

---
+++ front-ui/school-flow-ui/proxy.conf.mjs
@@ -0,0 +1,14 @@
+export default [
+  {
+    context: [
+      '/CLASS-SERVICE',
+      '/USER-SERVICE',
+      '/PROPERTY-SERVICE',
+      '/api',
+      '/oauth2',
+      '/login'
+    ],
+    target: 'http://localhost:8800',
+    secure: true
+  }
+]
--- front-ui/school-flow-ui/src/app/app.module.ts
+++ front-ui/school-flow-ui/src/app/app.module.ts
@@ -1,21 +1,49 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './layout/header/header.component';
+import { NavbarComponent } from './layout/header/navbar/navbar.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'school-flow-realm',
+        clientId: 'school-flow-client'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    KeycloakAngularModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
   ],
   bootstrap: [AppComponent]
 })
