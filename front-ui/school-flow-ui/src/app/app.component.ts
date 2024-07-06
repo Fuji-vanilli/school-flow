92f76fad6b75b7f3d3441a624c80fc756f01a8ae
Fuji-Vanilli <ijufillivan@gmail.com>
2024-07-06 12:09:10 +0000

    config ui angular

--- front-ui/school-flow-ui/src/app/app.component.ts
+++ front-ui/school-flow-ui/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'school-flow-ui';
+export class AppComponent implements OnInit{
+
+  faIconLibrary= inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
 }
---
+++ front-ui/school-flow-ui/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,19 @@
+import {
+  faCartPlus,
+  faCartShopping,
+  faCircleQuestion,
+  faCircleUser,
+  faGear, faMagnifyingGlass,
+  faRightFromBracket,
+  IconDefinition
+} from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[]= [
+  faRightFromBracket,
+  faGear,
+  faCircleUser,
+  faCircleQuestion,
+  faMagnifyingGlass,
+  faCartPlus,
+  faCartShopping
+]
