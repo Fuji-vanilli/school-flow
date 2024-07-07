f1bb2637b78cd0ecd40c8b34abdc588f5fee8aa3
Fuji-Vanilli <ijufillivan@gmail.com>
2024-07-07 16:57:16 +0000

    update front student

--- front-ui/school-flow-ui/src/app/admin/add-student/add-student.component.ts
+++ front-ui/school-flow-ui/src/app/admin/add-student/add-student.component.ts
@@ -2,6 +2,9 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StudentService } from '../../services/student.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { ClassService } from '../../services/class.service';
+import { Class } from '../../models/class.model';
 
 @Component({
   selector: 'app-add-student',
@@ -15,8 +18,16 @@ export class AddStudentComponent implements OnInit{
   studentService= inject(StudentService);
   formBuilder= inject(FormBuilder);
   router= inject(Router);
+  classService= inject(ClassService);
+
+  classes: Class[] | undefined;
 
   ngOnInit(): void {
+    this.initFormGroup();
+    this.loadClasses();
+  }
+
+  initFormGroup() {
     this.formGroup= this.formBuilder.group({
       firstname: this.formBuilder.control('', Validators.required),
       lastname: this.formBuilder.control('', Validators.required),
@@ -27,6 +38,18 @@ export class AddStudentComponent implements OnInit{
       address: this.formBuilder.control('', Validators.required),
       aClass: this.formBuilder.control('', Validators.required),
       originSchool: this.formBuilder.control('', Validators.required),
+    })
+  }
+
+  loadClasses() {
+    this.classService.getAll().subscribe({
+      next: response=> {
+        this.classes= response;
+      },
+      error: err=> {
+        console.log('error: ', err);
+        
+      }
     })
   }
 
@@ -46,6 +69,7 @@ export class AddStudentComponent implements OnInit{
     this.studentService.create(student).subscribe({
       next: response=> {
         console.log('new class created successfully');
+        Swal.fire('Succes', 'Nouvel élèves ajouté avec succès', 'success');
         this.router.navigateByUrl('/admin/class')
       },
       error: err=> {
