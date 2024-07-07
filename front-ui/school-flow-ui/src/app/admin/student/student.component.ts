3222d8e7dadd91131507150f8050fcc9f6a1c174
Fuji-Vanilli <ijufillivan@gmail.com>
2024-07-07 18:44:05 +0000

    update

--- front-ui/school-flow-ui/src/app/admin/student/student.component.ts
+++ front-ui/school-flow-ui/src/app/admin/student/student.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { StudentService } from '../../services/student.service';
+import { Student } from '../../models/student.model';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrl: './student.component.scss'
 })
-export class StudentComponent {
+export class StudentComponent implements OnInit {
+  studentService= inject(StudentService);
+
+  students: Student[] | undefined;
+
+  ngOnInit(): void {
+    this.loadStudents();
+  }
+
+  loadStudents() {
+    this.studentService.getAll().subscribe({
+      next: response=> {
+        this.students= this.students;
+        console.log('students: ', this.students);
+        
+      },
+      error: err=> {
+        console.log('error: ', err);
+        
+      }
+    })
+  }
+
+  update() {
+
+  }
+
+  delete() {
+    
+  }
 
 }
