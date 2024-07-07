ba108ad8c1035140fe06972751476f28a20d513b
Fuji-Vanilli <ijufillivan@gmail.com>
2024-07-07 14:48:36 +0000

    add student-service

---
+++ services/student-service/src/test/java/com/fuji/student_service/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuji.student_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
