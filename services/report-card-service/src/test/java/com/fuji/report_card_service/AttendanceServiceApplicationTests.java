3c839014a6c1ac869c46f8acd559982548a72f96
Fuji-Vanilli <ijufillivan@gmail.com>
2024-07-16 09:47:52 +0000

    add report-card service

---
+++ services/report-card-service/src/test/java/com/fuji/report_card_service/AttendanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuji.report_card_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
