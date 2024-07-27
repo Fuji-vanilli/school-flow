e94853f7960c909324c5058c01523508e787e5c3
Fuji-Vanilli <ijufillivan@gmail.com>
2024-07-27 11:25:03 +0000

    update course

--- front-ui/school-flow-ui/src/app/admin/course/course.component.ts
+++ front-ui/school-flow-ui/src/app/admin/course/course.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-course',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './course.component.scss'
 })
 export class CourseComponent implements OnInit{
+
+  formGroup: FormGroup | undefined;
+  formBuilder= inject(FormBuilder);
+
   ngOnInit(): void {
-   
+   this.initFormGroup();
+  }
+
+  initFormGroup() {
+    this.formGroup= this.formBuilder.group({
+      title: this.formBuilder.control('', Validators.required),
+      credit: this.formBuilder.control('', Validators.required),
+      professorID: this.formBuilder.control(''),
+      hoursPerWeek: this.formBuilder.control('', Validators.required),
+      pricePerHour: this.formBuilder.control('', Validators.required)
+    })
   }
 
 }
