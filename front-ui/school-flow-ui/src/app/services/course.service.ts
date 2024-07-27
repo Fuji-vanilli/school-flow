0dc593c9d5b809475c67098fe3874c91a6480f85
Fuji-Vanilli <ijufillivan@gmail.com>
2024-07-27 11:06:35 +0000

    course service front

--- front-ui/school-flow-ui/src/app/services/course.service.ts
+++ front-ui/school-flow-ui/src/app/services/course.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Course } from '../models/course.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
+  httpClient= inject(HttpClient);
+  url: string= environment.API_URL+'/COURSE-SERVICE/api/course';
+
   constructor() { }
+
+  create(course: Course): Observable<any> {
+    return this.httpClient.put(this.url+'/create', course);
+  }
+
+  update(course: Course): Observable<any> {
+    return this.httpClient.put(this.url+'/update', course);
+  }
+
+  get(id: string): Observable<any> {
+    return this.httpClient.get(this.url+'/get/'+id);
+  }
+
+  getAll(): Observable<any> {
+    return this.httpClient.get(this.url+'/all');
+  }
 }
