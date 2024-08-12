d6e5d77fc2ac836fddf1dd304630da8b231600c9
Fuji-Vanilli <ijufillivan@gmail.com>
2024-08-12 10:29:31 +0000

    update

--- front-ui/school-flow-ui/src/app/pages/admin/dashboard/dashboard.component.ts
+++ front-ui/school-flow-ui/src/app/pages/admin/dashboard/dashboard.component.ts
@@ -17,9 +17,10 @@ export class DashboardComponent implements OnInit{
   classService= inject(ClassService);
 
   classes: Class[] | undefined;
+  classResult: Class[] | undefined;
 
   length = 50;
-  pageSize = 10;
+  pageSize = 5;
   pageIndex = 0;
   pageSizeOptions = [5, 10, 25];
 
@@ -39,6 +40,7 @@ export class DashboardComponent implements OnInit{
     this.classService.getAll().subscribe({
       next: classes=> {
         this.classes= classes; 
+        this.length= this.classes?.length!;
         console.log("classes: ", classes); 
         console.timeEnd('loadClasses')
         
@@ -55,6 +57,11 @@ export class DashboardComponent implements OnInit{
     this.length = e.length;
     this.pageSize = e.pageSize;
     this.pageIndex = e.pageIndex;
+
+    const startIndex= this.pageIndex*this.pageSize;
+    const endIndex= startIndex+ this.pageSize;
+
+    this.classResult= this.classes?.slice(startIndex, endIndex);
   }
 
   setPageSizeOptions(setPageSizeOptionsInput: string) {
