e767072aca9633a8abc4ec894f5301f1aedce42a
Fuji-Vanilli <ijufillivan@gmail.com>
2024-08-13 19:47:59 +0000

    update

--- front-ui/school-flow-ui/src/app/admin/professor/professor.component.ts
+++ front-ui/school-flow-ui/src/app/admin/professor/professor.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Professor } from '../../models/professor.model';
+import { ProfessorService } from '../../services/professor.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-professor',
   templateUrl: './professor.component.html',
   styleUrl: './professor.component.scss'
 })
-export class ProfessorComponent {
+export class ProfessorComponent implements OnInit {
+  professors: Professor[] | undefined;
+  professorService= inject(ProfessorService);
+  formBuilder= inject(FormBuilder);
+
+  formGroup!: FormGroup;
+
+  ngOnInit(): void {
+    this.initFormGroup();
+    this.loadProfessors();
+  }
+
+  loadProfessors() {
+    this.professorService.getAll().subscribe({
+      next: response=> {
+        this.professors= response;
+        console.log('professors: ', this.professors);
+        
+      },
+      error: err=> {
+        console.log(err);
+        
+      }
+    })
+  }
+
+  initFormGroup() {
+    this.formGroup= this.formBuilder.group({
+
+    })
+  }
+
+  create() {
+
+  }
+
+  updateProfessor() {
+    
+  }
+  update(professor: Professor) {
+
+  }
+
+  delete(id: string) {
+
+  }
+
+  capitalize(s: string): string {
+    return s.charAt(0).toUpperCase()+s.slice(1);
+  }
 
 }
--- front-ui/school-flow-ui/src/app/models/professor.model.ts
+++ front-ui/school-flow-ui/src/app/models/professor.model.ts
@@ -1,3 +1,16 @@
+import { Class } from "./class.model";
+import { Course } from "./course.model";
+
 export interface Professor {
-    
+    id?: string,
+    matricule?: string,
+    firstname?: string,
+    lastname?: string,
+    dateOfBirth?: Date,
+    email?: string,
+    phone?: string,
+    classIds?: string[],
+    classes?: Array<Class>,
+    courseId?: string,
+    Course?: Course
 }
