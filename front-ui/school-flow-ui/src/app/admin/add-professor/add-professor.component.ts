93b45fc54e64713d2ccb28919c1a87de60ed89f5
Fuji-vanilli <ijufillivan@gmail.co>
2024-08-15 12:31:29 +0000

    update

--- front-ui/school-flow-ui/src/app/admin/add-professor/add-professor.component.ts
+++ front-ui/school-flow-ui/src/app/admin/add-professor/add-professor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-professor',
@@ -6,14 +7,20 @@ import { Component, OnInit, signal } from '@angular/core';
   styleUrl: './add-professor.component.scss'
 })
 export class AddProfessorComponent implements OnInit{
-  
+  formBuilder= inject(FormBuilder);
+
+  formGroup!: FormGroup;
+
   ngOnInit(): void {
     
   }
-  hide = signal(true);
-  
-  clickEvent(event: MouseEvent) {
-    this.hide.set(!this.hide());
-    event.stopPropagation();
+
+  initFormGroup() {
+    this.formGroup= this.formBuilder.group({
+      firstname: this.formBuilder.control('', Validators.required),
+      lastname: this.formBuilder.control(''),
+      dateOfBirth: this.formBuilder.control('', Validators.required),
+      
+    })
   }
 }
